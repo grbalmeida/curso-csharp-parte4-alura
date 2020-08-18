67dd31a5f83e5f17989c51982c3f0fa4a91f2223
gilvan <g.r.almeida@live.com>
2020-08-18 10:33:04 -0300

    Lidando com erros

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -6,9 +6,27 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(7480, 874150);
+            Metodo();
+            Console.ReadLine();
+        }
 
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
         }
     }
 }
